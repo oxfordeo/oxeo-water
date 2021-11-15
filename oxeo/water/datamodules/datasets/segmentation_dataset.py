57c7876fe7fffbc9c26b6e1ac94b5a58cbda3f23
Francisco Dorr <fran.dorr@gmail.com>
2021-11-15 18:49:29 +0000

    Add unet2d training scripts and arch

---
+++ oxeo/water/datamodules/datasets/segmentation_dataset.py
@@ -0,0 +1,35 @@
+from typing import Callable, List, Optional
+
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class SegmentationDataset(Dataset):
+    """A dataset that reads from cache in local disk."""
+
+    def __init__(
+        self,
+        patch_paths: List[str],
+        transform: Optional[Callable] = None,
+    ):
+        super().__init__()
+        self.transform = transform
+        self.patch_paths = patch_paths
+
+    def __getitem__(self, index: int):
+        patch_path = self.patch_paths[index]
+
+        data = np.load(f"{patch_path}/data.npz")["arr_0"].astype(np.int16)
+        label = np.load(f"{patch_path}/weak_labels.npz")["arr_0"]
+        sample = {
+            "data": data,
+            "label": label,
+        }
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def __len__(self):
+        return len(self.patch_paths)
