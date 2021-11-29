12c4f28e42061e865bbefdf8c64fba743cff767d
Francisco Dorr <fran.dorr@gmail.com>
2021-11-29 21:50:42 +0000

    New virtual dataset supporting multiprocessing

--- oxeo/water/datamodules/datasets/virtual_dataset.py
+++ oxeo/water/datamodules/datasets/virtual_dataset.py
@@ -1,98 +1,72 @@
-from datetime import datetime
-from typing import Callable, List, Optional
+from typing import Callable, Dict, List, Optional, Tuple
 
-import xarray as xr
+import gcsfs
+from fsspec import asyn
+from satools.io import ConstellationData, load_virtual_datasets
 from torch.utils.data import Dataset
 
 
 class VirtualDataset(Dataset):
-    """A dataset that reads from cache in local disk."""
+    """A dataset that reads from xarray datasets."""
 
     def __init__(
         self,
-        data_virtual_datasets: List[xr.Dataset],
-        label_virtual_datasets: List[xr.Dataset] = None,
+        constellation_regions: Dict[str, List[List[ConstellationData]]],
         patch_size: int = 100,
-        start_date: datetime = None,
-        end_date: datetime = None,
+        preprocess: Optional[Callable] = None,
         transform: Optional[Callable] = None,
+        index_map: List[Tuple[int, str, int, int]] = None,
     ):
+        """Pytorch Dataset to load from virtual xarray datasets
+
+        Args:
+            constellation_regions (Dict[str, List[List[ConstellationData]]]): a dictionary that must contain a 'data' key and
+                                an optional 'label' key. The values should be a list of lists of ConstellationData that
+                                will be used to build xarrays.
+            preprocess (Optional[Callable], optional): Preprocess to apply to the resulting sample. Defaults to None.
+            transform (Optional[Callable], optional): Transforms to apply to the resulting sample. Defaults to None.
+            index_map: List[Tuple[int, str, int, int]]: Index to each patch in the datasets. Use create_index_map function
+                                to create it. It must be done outside of the VirtualDataset because otherwise
+                                there are tilt problems with ffspec in multiprocessing.
+
+        """
         super().__init__()
-        self.data_virtual_datasets = [
-            ds.sel({"revisits": slice(start_date, end_date)})
-            for ds in data_virtual_datasets
-        ]
-        self.label_virtual_datasets = label_virtual_datasets
-        if self.label_virtual_datasets is not None:
-            self.label_virtual_datasets = [
-                ds.sel({"revisits": slice(start_date, end_date)})
-                for ds in label_virtual_datasets
-            ]
 
-            self.data_virtual_datasets = [
-                ds.sel({"revisits": label_virtual_datasets[i]["revisits"]})
-                for i, ds in enumerate(data_virtual_datasets)
-            ]
-
+        self.constellation_regions = constellation_regions
         self.patch_size = patch_size
-        self.start_date = start_date
-        self.end_date = end_date
+        self.preprocess = preprocess
         self.transform = transform
-        self.index_map = []
-        for dataset_index in range(len(data_virtual_datasets)):
-            timestamps = self.data_virtual_datasets[dataset_index]["revisits"].data
-            for timestamp_index in timestamps:
-                for i in range(
-                    self.data_virtual_datasets[dataset_index].dims["height"]
-                    // self.patch_size
-                ):
-                    for j in range(
-                        self.data_virtual_datasets[dataset_index].dims["width"]
-                        // self.patch_size
-                    ):
-                        self.index_map.append((dataset_index, timestamp_index, i, j))
+        self.index_map = index_map
+
+        dates = [item[1] for item in self.index_map]
+        self.date_range = (min(dates), max(dates))
 
     def __getitem__(self, index: int):
-
         dataset_index, timestamp_index, i, j = self.index_map[index]
 
-        data = (
-            self.data_virtual_datasets[dataset_index]
-            .sel({"revisits": timestamp_index})
-            .isel(
-                {
-                    "height": slice(
-                        i * self.patch_size, i * self.patch_size + self.patch_size
-                    ),
-                    "width": slice(
-                        j * self.patch_size, j * self.patch_size + self.patch_size
-                    ),
-                }
-            )
-        )
+        # This is only if using this class without datamodule
+        if not hasattr(self, "data_virtual_datasets"):
+            (
+                self.data_virtual_datasets,
+                self.label_virtual_datasets,
+            ) = load_virtual_datasets(self.constellation_regions, self.date_range, None)
 
+        data = self.get_dataset_patch(
+            self.data_virtual_datasets[dataset_index], timestamp_index, i, j
+        )
         sample = {
             "data": data,
         }
 
         if self.label_virtual_datasets is not None:
 
-            label = (
-                self.label_virtual_datasets[dataset_index]
-                .sel({"revisits": timestamp_index})
-                .isel(
-                    {
-                        "height": slice(
-                            i * self.patch_size,
-                            i * self.patch_size + self.patch_size,
-                        ),
-                        "width": slice(
-                            j * self.patch_size, j * self.patch_size + self.patch_size
-                        ),
-                    }
-                )
+            label = self.get_dataset_patch(
+                self.label_virtual_datasets[dataset_index], timestamp_index, i, j
             )
             sample["label"] = label
+
+        if self.preprocess:
+            sample = self.preprocess(sample)
 
         if self.transform:
             sample = self.transform(sample)
@@ -101,3 +75,37 @@ class VirtualDataset(Dataset):
 
     def __len__(self):
         return len(self.index_map)
+
+    def per_worker_init(self, worker_id: int = 0) -> None:
+        """
+        This is needed to work with gcp and pytorch datamodules.
+        Otherwise it won't work when num_workers > 0
+        """
+        asyn.iothread[0] = None
+        asyn.loop[0] = None
+
+        gcs = gcsfs.GCSFileSystem(
+            access="read_only",
+            # skip_instance_cache=True  # Why skip_instance_cache?  See https://github.com/dask/gcsfs/issues/379#issuecomment-839929801
+        )
+        fs_mapper = gcs.get_mapper
+
+        """Called by worker_init_fn on each copy of VirtualDataset after the worker process has been spawned."""
+        (
+            self.data_virtual_datasets,
+            self.label_virtual_datasets,
+        ) = load_virtual_datasets(
+            self.constellation_regions, self.date_range, fs_mapper
+        )
+
+    def get_dataset_patch(self, ds, timestamp_index, i, j):
+        return ds.sel({"revisits": timestamp_index}).isel(
+            {
+                "height": slice(
+                    i * self.patch_size, i * self.patch_size + self.patch_size
+                ),
+                "width": slice(
+                    j * self.patch_size, j * self.patch_size + self.patch_size
+                ),
+            }
+        )
