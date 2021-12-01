564d7f1ae31de2d44ad0a432e6ab9fd1ade10f04
Chris Arderne <chris@rdrn.me>
2021-12-01 13:18:07 +0200

    Simplify Pekel data changes, use satextractor band order

--- oxeo/water/models/pekel/utils.py
+++ oxeo/water/models/pekel/utils.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import List, Optional
 
 import numpy as np
 from attr import define
 from rasterio.fill import fillnodata
+from satextractor.models.constellation_info import BAND_INFO
 from skimage.color import rgb2hsv
 from skimage.morphology import erosion
 
@@ -24,77 +25,76 @@ class PekelBands:
     tirs1: Optional[np.ndarray] = None
 
 
-def pekel_bands(arr: np.ndarray, constellation) -> PekelBands:
+def get_band_list(constellation: str) -> List[str]:
+    return [b["band"].common_name for b in BAND_INFO[constellation].values()]
+
+
+def pekel_bands(arr: np.ndarray, constellation: str) -> PekelBands:
     """Get all bands needed for Pekel masks."""
 
     assert arr.ndim == 3, "arr must have ndim == 3"
 
-    cloud = cloud_band(constellation)
-    bands = ["red", "green", "blue", "nir", "swir1", "swir2", "derived:ndvi", "alpha"]
-    bands.append(cloud)
+    bands = get_band_list(constellation)
+
+    ndvi = (arr[bands.index("nir")] - arr[bands.index("red")]) / (
+        arr[bands.index("nir")] + arr[bands.index("red")]
+    )
+    alpha = np.ones(arr.shape[1:])
+    cloud_mask = np.zeros(arr.shape[1:])
 
     # Scale sat-extractor bands to 0-1
     for i in range(6):
+        a = np.clip(arr[i, ...], 0, 10000)
+        arr[i, ...] = np.interp(a, (0, 10000), (0, 1))
 
-        a = np.clip(arr[..., i], 0, 10000)
-        arr[..., i] = np.interp(a, (0, 10000), (0, 1))
-
-    arr = fill_nodata_single(arr, alpha_band=bands.index("alpha"), erode=True)
+    # Don't fill_nodata, as we don't have a real alpha band!
+    # arr = fill_nodata_single(arr, alpha, erode=True)
 
     hsv = rgb2hsv(
-        arr[..., [bands.index("swir2"), bands.index("nir"), bands.index("red")]]
+        arr[
+            [bands.index("swir2"), bands.index("nir"), bands.index("red")], ...
+        ].transpose(1, 2, 0)
     )
     hsv2 = rgb2hsv(
-        arr[..., [bands.index("nir"), bands.index("green"), bands.index("blue")]]
+        arr[
+            [bands.index("nir"), bands.index("green"), bands.index("blue")], ...
+        ].transpose(1, 2, 0)
     )
 
     b = PekelBands(
-        alpha=arr[..., bands.index("alpha")],
-        blue=arr[..., bands.index("blue")],
-        swir1=arr[..., bands.index("swir1")],
-        swir2=arr[..., bands.index("swir2")],
+        alpha=alpha,
+        blue=arr[bands.index("blue"), ...],
+        swir1=arr[bands.index("swir1"), ...],
+        swir2=arr[bands.index("swir2"), ...],
         hue=hsv[..., 0] * 360,
         sat=hsv[..., 1],
         val=hsv[..., 2],
         hue2=hsv2[..., 0] * 360,
         sat2=hsv2[..., 1],
         val2=hsv2[..., 2],
-        cloud=arr[..., bands.index(cloud)],
-        ndvi=arr[..., bands.index("derived:ndvi")],
+        cloud=cloud_mask,
+        ndvi=ndvi,
+        tirs1=arr[bands.index("tirs1")] if "tirs1" in bands else None,
     )
-    if "sentinel" not in constellation:
-        b.tirs1 = arr[..., bands.index("tirs1")] + 273.15
 
     return b
 
 
-def fill_nodata_single(frame, alpha_band, erode=False):
-    """Fillnodata on all bands UNTIL alpha band."""
+def fill_nodata_single(arr, alpha, erode=False):
+    """Fillnodata on all bands."""
 
-    orig_dtype = frame.dtype
-    frame = frame.copy().astype(np.float32)
-    sel = np.where(frame[:, :, alpha_band] == 0)
-    frame[:, :, :alpha_band][sel] = -1
+    orig_dtype = arr.dtype
+    frame = arr.copy().astype(np.float32)
+    sel = np.where(alpha == 0)
+    frame[sel] = -1
 
-    for b in range(alpha_band):
-        band = frame[:, :, b].copy()
+    for b in range(frame.shape[0]):
+        band = frame[b, ...].copy()
         mask = (band != -1).astype(np.int8)
 
         if erode:
             # Do an erosion to cover artifacts on the edge of nodata sections
             mask = erosion(mask, selem=np.ones((3, 3)))
         band = fillnodata(band, mask)
-        frame[:, :, b] = band.copy()
+        frame[b, ...] = band.copy()
     return frame.astype(orig_dtype)
-
-
-def cloud_band(contellation):
-    """Get the suitable cloud band for the given scene ID."""
-
-    if "LT04" in contellation or "LT05" in contellation or "LE07" in contellation:
-        cloud = "derived:visual_cloud_mask"
-    elif "LC08" in contellation or "sentinel-2" in contellation:
-        cloud = "cloud-mask"
-    else:
-        raise NotImplementedError(f"No cloud band set for for {contellation}")
-    return cloud
