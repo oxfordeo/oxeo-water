c743374daf15ca6090f03adcda088c71bdaaa390
Chris Arderne <chris@rdrn.me>
2021-12-03 15:07:10 +0200

    Change osm_mask ndim; add prints/comments

--- oxeo/water/metrics/metrics.py
+++ oxeo/water/metrics/metrics.py
@@ -33,13 +33,20 @@ def segmentation_area_multiple(
 
     dfs = []
     for tsm in segs:
+        print(f"Calulating metrics for {tsm.constellation}")
+        # osm_raster must be created separately for each constellation
+        # as they have different resolutions
         shape = tsm.data.shape[2:]
         epsg = tiles[0].epsg
         osm_raster = rasterize_geom(geom=geom, tiles=tiles, shape=shape, epsg=epsg)
+        print(f"Created OSM mask with {osm_raster.shape=}")
 
         data = mask_cube(tsm.data, osm_raster)
+        print(f"Masked data cube with {data.shape=}")
 
         area = segmentation_area(data, unit="meter", resolution=tsm.resolution)
+        print(f"Calculated 1D area array with {area.shape=}")
+
         df = pd.DataFrame(
             data={
                 "date": tsm.data.revisits.compute().data,
@@ -48,28 +55,39 @@ def segmentation_area_multiple(
             }
         )
         dfs.append(df)
+        print("Inserted into DF")
     return pd.concat(dfs, axis=0)
 
 
 def mask_single(arr: da.Array, i: int, geom_raster: np.ndarray):
+    # TODO will need to modify this once the incoming masks
+    # have multiple classes (land/water/cloud etc)
+    # Basically must just set all non-water to 0 and all water to 1
     lab = arr[i, 0, ...].compute().astype(bool)
     lab = closing(lab, square(3))
     lab = remove_small_holes(lab, area_threshold=50, connectivity=2)
     lab = remove_small_objects(lab, min_size=50, connectivity=2)
-    lab = label(lab, connectivity=2)
+    lab = label(lab, background=0, connectivity=2)
 
+    # Overlay the OSM mask and find the label IDs of all
+    # labelled water that it covers
     masked = geom_raster * lab
     keepers = [val for val in np.unique(masked) if val != 0]
 
+    # Go back to the labelled OSM mask and keep all pixels with those labels
     fin = np.isin(lab, keepers)
-    da_arr = da.stack([da.from_array(fin)], axis=0)
+    da_arr = da.from_array(fin)
     return da_arr
 
 
 def mask_cube(data: xr.DataArray, osm_raster: np.ndarray) -> xr.DataArray:
+    # TODO Probably tere's some clever Dasky stuff to do here
+    # Right now it's just a sequential loop
     all_masks = [mask_single(data, i, osm_raster) for i in range(len(data))]
     block = da.stack(all_masks, axis=0)
-    osm_masked = xr.DataArray(block, dims=["revisits", "bands", "height", "width"])
+    # Dropped the 'band' dimension as not needed
+    # Might be better to keep it for consistency with other stuff?
+    osm_masked = xr.DataArray(block, dims=["revisits", "height", "width"])
     return osm_masked
 
 
@@ -105,7 +123,7 @@ def pearson(x, y):
 def rasterize_geom(
     geom: Union[Polygon, MultiPolygon],
     tiles: List[Tile],
-    shape: Tuple(int, int),
+    shape: Tuple[int, int],
     epsg: int,
 ) -> np.ndarray:
     geom = gpd.GeoSeries(geom, crs=CRS.from_epsg(4326)).to_crs(epsg=epsg).geometry
