1421b8ad983451aff5bc853187a330b3453549aa
Francisco Dorr <fran.dorr@gmail.com>
2021-12-13 14:32:36 -0300

    Feature/cloud detection (#31)
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix package namespace installation
    
    * Fix some types and defaults
    
    * Temp: Depend on oxfordeo sat-extractor fork
    
    * Deal with changed tile naming
    
    * Add satools req
    
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix some types and defaults
    
    * Add satools req
    
    * add virtual dataset
    
    * Add module to init
    
    * Add transforms
    
    * Add constellation datamodule
    
    * New virtual dataset supporting multiprocessing
    
    * rm seg datamodule
    
    * Update notebook with datamodule
    
    * add modules to import
    
    * Add select bandas transform
    
    * Modify training script to support xarray dm
    
    * Add iterable dataset
    
    * Fix training
    
    * Interp only for sentinel
    
    * Add oxeo satools
    
    * Add satools processing to toa
    
    * Add public api to init
    
    * Modify toa function name
    
    * add hollstein trees
    
    * Add pekel predictor with toa
    
    * fix to work with sen2 als
    
    * Remove imports
    
    * Add example notebook
    
    * Change pekel to zarr to keep same public api
    
    * Fix metadata path
    
    Co-authored-by: Chris Arderne <chris@rdrn.me>

--- oxeo/satools/io/__init__.py
+++ oxeo/satools/io/__init__.py
@@ -1,6 +1,1 @@
-from .io import (  # noqa
-    ConstellationData,
-    constellation_dataarray,
-    create_index_map,
-    load_virtual_datasets,
-)
+from .io import ConstellationData, create_index_map, load_virtual_datasets  # noqa
