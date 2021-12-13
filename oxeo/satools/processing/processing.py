aa3495b4fbb0b9a1043395d3cdf28fc93f13a776
Francisco Dorr <fran.dorr@gmail.com>
2021-12-13 11:45:12 -0300

    Feature/cloud detection (#30)
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix package namespace installation
    
    * Fix some types and defaults
    
    * Temp: Depend on oxfordeo sat-extractor fork
    
    * Deal with changed tile naming
    
    * Add satools req
    
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix some types and defaults
    
    * Add satools req
    
    * add virtual dataset
    
    * Add module to init
    
    * Add transforms
    
    * Add constellation datamodule
    
    * New virtual dataset supporting multiprocessing
    
    * rm seg datamodule
    
    * Update notebook with datamodule
    
    * add modules to import
    
    * Add select bandas transform
    
    * Modify training script to support xarray dm
    
    * Add iterable dataset
    
    * Fix training
    
    * Interp only for sentinel
    
    * Add oxeo satools
    
    * Add satools processing to toa
    
    * Add public api to init
    
    * Modify toa function name
    
    * add hollstein trees
    
    * Add pekel predictor with toa
    
    * fix to work with sen2 als
    
    * Remove imports
    
    * Add example notebook
    
    * Change pekel to zarr to keep same public api
    
    Co-authored-by: Chris Arderne <chris@rdrn.me>

--- oxeo/satools/processing/processing.py
+++ oxeo/satools/processing/processing.py
@@ -1,40 +1,82 @@
-from typing import List
-
-import attr
 import numpy as np
-import xarray as xr
+from satextractor.models.constellation_info import BAND_INFO
+
+from .mtl_parser import parsemeta
 
 
-@attr.s
-class BandTOAValues:
-    band = attr.ib()
-    radiance_mult = attr.ib(default=None)
-    radiance_add = attr.ib(default=None)
-    reflectance_mult = attr.ib(default=None)
-    reflectance_add = attr.ib(default=None)
-    k1_constant = attr.ib(default=None)
-    k2_constant = attr.ib(default=None)
+def to_toa_radiance(arr: np.ndarray, mtl_dict: dict, constellation: str):
 
-
-def to_toa_reflectance(arr: xr.DataArray, band_toa_values: List[BandTOAValues]):
+    bands = list(BAND_INFO[constellation].keys())
     arr = arr.astype(float)
-    for b_toa in band_toa_values:
-        arr.loc[dict(bands=b_toa.band)] = (
-            b_toa.reflectance_mult * arr.loc[dict(bands=b_toa.band)]
-            + b_toa.reflectance_add
-        )
+
+    radiometric_rescaling = mtl_dict["L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"]
+
+    for i, b in enumerate(bands):
+        radiance_add_band = f"RADIANCE_ADD_BAND_{b.replace('B','')}"
+        radiance_mult_band = f"RADIANCE_MULT_BAND_{b.replace('B','')}"
+
+        radiance_add = radiometric_rescaling.get(radiance_add_band)
+        radiance_mult = radiometric_rescaling.get(radiance_mult_band)
+
+        if radiance_add is not None and radiance_mult is not None:
+            arr[i] = radiance_mult * arr[i] + radiance_add
     return arr
 
 
-def to_toa_brightness_temp(arr: xr.DataArray, band_toa_values: List[BandTOAValues]):
+def to_toa_reflectance(arr: np.ndarray, mtl_dict: dict, constellation: str):
+    bands = list(BAND_INFO[constellation].keys())
     arr = arr.astype(float)
 
-    for b_toa in band_toa_values:
-        current_band = arr.loc[dict(bands=b_toa.band)]
-        toa_radiance = b_toa.radiance_mult * current_band + b_toa.radiance_add
+    radiometric_rescaling = mtl_dict["L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"]
 
-        arr.loc[dict(bands=b_toa.band)] = b_toa.k2_constant / np.log(
-            b_toa.k1_constant / toa_radiance + 1
-        )
+    sun_elevation = mtl_dict["L1_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SUN_ELEVATION"]
+    for i, b in enumerate(bands):
+        reflectance_add_band = f"REFLECTANCE_ADD_BAND_{b.replace('B','')}"
+        reflectance_mult_band = f"REFLECTANCE_MULT_BAND_{b.replace('B','')}"
+
+        reflectance_add = radiometric_rescaling.get(reflectance_add_band)
+        reflectance_mult = radiometric_rescaling.get(reflectance_mult_band)
+
+        if reflectance_add is not None and reflectance_mult is not None:
+            arr[i] = (reflectance_mult * arr[i] + reflectance_add) / np.sin(
+                sun_elevation
+            )
+    return arr
+
+
+def to_toa_brightness_temp(arr: np.ndarray, mtl_dict: dict, constellation: str):
+    bands = list(BAND_INFO[constellation].keys())
+    arr = arr.astype(float)
+
+    radiometric_rescaling = mtl_dict["L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"]
+
+    for i, b in enumerate(bands):
+        radiance_add_band = f"RADIANCE_ADD_BAND_{b.replace('B','')}"
+        radiance_mult_band = f"RADIANCE_MULT_BAND_{b.replace('B','')}"
+
+        k1_constant_band = f"K1_CONSTANT_BAND_{b.replace('B','')}"
+        k2_constant_band = f"K1_CONSTANT_BAND_{b.replace('B','')}"
+
+        k1 = radiometric_rescaling.get(k1_constant_band)
+        k2 = radiometric_rescaling.get(k2_constant_band)
+
+        radiance_add = radiometric_rescaling.get(radiance_add_band)
+        radiance_mult = radiometric_rescaling.get(radiance_mult_band)
+
+        if k1 is not None and k2 is not None:
+
+            rad = radiance_mult * arr[i] + radiance_add
+
+            arr[i] = k2 / np.log(k1 / rad + 1)
 
     return arr
+
+
+def to_toa(arr: np.ndarray, mtl_dict: dict, constellation: str):
+    ref = to_toa_reflectance(arr, mtl_dict, constellation)
+    return to_toa_brightness_temp(ref, mtl_dict, constellation)
+
+
+def get_mtl_dict(fs, mtl_path: str, revisit: int):
+    f = fs.open([path for path in fs.ls(mtl_path)][revisit])
+    return parsemeta(f.read().decode("utf-8"))
--- oxeo/water/models/pekel/pekel.py
+++ oxeo/water/models/pekel/pekel.py
@@ -1,10 +1,12 @@
 from typing import Any
 
+import zarr
 from attr import define
 from joblib import Parallel, delayed
 from satextractor.utils import tqdm_joblib
 from tqdm import tqdm
 
+from oxeo.satools.processing import get_mtl_dict, to_toa
 from oxeo.water.models import Predictor
 from oxeo.water.models.pekel import masks, utils
 
@@ -13,31 +15,37 @@ from oxeo.water.models.pekel import masks, utils
 class PekelPredictor(Predictor):
     def predict_single_revisit(
         self,
-        arr: Any,
+        arr: zarr.core.Array,
         revisit: int,
         constellation="sentinel-2",
-        compute=False,
     ):
         """Return mask for a BxHxW array
         Args:
-            arr (np.ndarray): the array to make predictions in
+            arr (zarr.core.Array): the zarr array to make predictions in
 
         Returns:
             [type]: HxW mask
         """
-        arr = arr[revisit]
-        if compute:
-            arr = arr.compute()
+        fs = arr.store.fs
 
-        p_bands = utils.pekel_bands(arr, constellation)
-        c_masks = masks.combine_masks(p_bands, False)
+        if "landsat" in constellation:
+            mtl_path = "/".join(arr.store.dir_path().split("/")[:-2] + ["metadata"])
+            mtl_dict = get_mtl_dict(fs, mtl_path, revisit)
+            arr = to_toa(arr[revisit], mtl_dict, constellation)
+        else:
+            arr = arr[revisit]
+
+        p_bands = utils.pekel_bands(
+            arr,
+            constellation,
+        )
+        c_masks = masks.combine_masks(p_bands, cloud_mask=False)
         return c_masks
 
     def predict(
         self,
         arr: Any,
         constellation="sentinel-2",
-        compute=False,
         n_jobs=-1,
         verbose=0,
     ):
@@ -56,9 +64,7 @@ class PekelPredictor(Predictor):
         ):
             masks = Parallel(n_jobs=n_jobs, verbose=verbose)(
                 [
-                    delayed(self.predict_single_revisit)(
-                        arr, revisit, constellation, compute
-                    )
+                    delayed(self.predict_single_revisit)(arr, revisit, constellation)
                     for revisit in range(arr.shape[0])
                 ],
             )
