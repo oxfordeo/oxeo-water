ca63fb2c2381a0e65d1f82e47ad066a9dd90ead1
Francisco Dorr <fran.dorr@gmail.com>
2021-12-14 15:47:54 -0300

    Feature/cloud detection (#32)
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix package namespace installation
    
    * Fix some types and defaults
    
    * Temp: Depend on oxfordeo sat-extractor fork
    
    * Deal with changed tile naming
    
    * Add satools req
    
    * Add data retrieve support
    
    * Add unet2d training scripts and arch
    
    * Ready to train unet
    
    * numpy without compression
    
    * Add full lake prediction from zarr
    
    * Add unet metrics
    
    * Fix some types and defaults
    
    * Add satools req
    
    * add virtual dataset
    
    * Add module to init
    
    * Add transforms
    
    * Add constellation datamodule
    
    * New virtual dataset supporting multiprocessing
    
    * rm seg datamodule
    
    * Update notebook with datamodule
    
    * add modules to import
    
    * Add select bandas transform
    
    * Modify training script to support xarray dm
    
    * Add iterable dataset
    
    * Fix training
    
    * Interp only for sentinel
    
    * Add oxeo satools
    
    * Add satools processing to toa
    
    * Add public api to init
    
    * Modify toa function name
    
    * add hollstein trees
    
    * Add pekel predictor with toa
    
    * fix to work with sen2 als
    
    * Remove imports
    
    * Add example notebook
    
    * Change pekel to zarr to keep same public api
    
    * Fix metadata path
    
    * Add cloud mask predictor
    
    Co-authored-by: Chris Arderne <chris@rdrn.me>

---
+++ oxeo/water/models/cloud_mask/__init__.py
@@ -0,0 +1,1 @@
+from .cloud_mask import CloudMaskPredictor  # noqa
--- oxeo/water/models/factory.py
+++ oxeo/water/models/factory.py
@@ -1,4 +1,5 @@
 from oxeo.water.models import ModelDef
+from oxeo.water.models.cloud_mask import CloudMaskPredictor
 from oxeo.water.models.pekel import PekelPredictor
 
 
@@ -6,6 +7,9 @@ def model_factory(name: str) -> ModelDef:
     model_list = {
         "pekel": ModelDef(
             predictor=PekelPredictor,
+        ),
+        "cloud_mask": ModelDef(
+            predictor=CloudMaskPredictor,
         ),
     }
     return model_list[name]
