6ecd1b153092e6b11559530243a664ace86cb134
Francisco Dorr <fran.dorr@gmail.com>
2022-01-04 12:58:38 -0300

    Add tqdm parallel segmentation multi (#43)

--- oxeo/water/utils/utils.py
+++ oxeo/water/utils/utils.py
@@ -1,12 +1,35 @@
+import contextlib
 import logging
 import warnings
 from typing import List, Sequence
 
+import joblib
 import pytorch_lightning as pl
 import rich.syntax
 import rich.tree
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.utilities import rank_zero_only
+
+
+@contextlib.contextmanager
+def tqdm_joblib(tqdm_object):
+    """Context manager to patch joblib to report into tqdm progress bar given as argument"""
+
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
 
 
 def get_logger(name=__name__) -> logging.Logger:
