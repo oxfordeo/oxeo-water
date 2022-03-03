6a730f71b6493ba7681850c3d903b41ed9789383
Chris Arderne <chris@rdrn.me>
2022-03-03 16:16:05 +0000

    Move area_reduction to separate module

--- oxeo/water/metrics/__init__.py
+++ oxeo/water/metrics/__init__.py
@@ -1,6 +1,8 @@
 from .metrics import (  # noqa
-    reduce_to_area,
-    seg_area_all,
     tile_stat_per_band,
     pearson,
+)
+from .area_reduction import (  # noqa
+    reduce_to_area,
+    seg_area_all,
 )
