5cda32cfcce140b986971cdebb5896d4507685e3
Francisco Dorr <fran.dorr@gmail.com>
2022-03-07 14:17:51 +0000

    Add make_paths tests

--- oxeo/core/tests/test_models.py
+++ oxeo/core/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 
 from oxeo.core.models import tile
 
-a_tile = tile.Tile(
+square_tile = tile.Tile(
     zone=44,
     row="Q",
     min_x=780000,
@@ -12,11 +12,27 @@ a_tile = tile.Tile(
     epsg=32644,
 )
 
+non_square_tile = tile.Tile(
+    zone=44,
+    row="Q",
+    min_x=780000,
+    min_y=2360000,
+    max_x=790000,
+    max_y=2380000,
+    epsg=32644,
+)
+
+sentinel2_tile_path = tile.TilePath(square_tile, "sentinel-2", "gs://oxeo-water/test")
+landsat8_tile_path = tile.TilePath(square_tile, "landsat-8", "gs://oxeo-water/test")
+
+
+# tile_id tests
+
 
 @pytest.mark.parametrize(
     "tile_id, expected",
     [
-        ("44_Q_10000_78_237", a_tile),
+        ("44_Q_10000_78_237", square_tile),
     ],
 )
 def test_tile_from_id_ok(tile_id, expected):
@@ -33,3 +49,58 @@ def test_tile_from_id_ok(tile_id, expected):
 def test_tile_from_id_error(tile_id, expected):
     with pytest.raises(expected):
         tile.tile_from_id(tile_id)
+
+
+# get_tile_size tests
+
+
+@pytest.mark.parametrize(
+    "tiles, expected",
+    [
+        ([square_tile], 10000),
+    ],
+)
+def test_get_tile_size_ok(tiles, expected):
+    assert tile.get_tile_size(tiles) == expected
+
+
+@pytest.mark.parametrize(
+    "tiles, expected",
+    [
+        ([non_square_tile], AssertionError),
+        ([square_tile, non_square_tile], AssertionError),
+    ],
+)
+def test_get_tile_size_error(tiles, expected):
+    with pytest.raises(expected):
+        tile.get_tile_size(tiles)
+
+
+# make_paths tests
+
+
+@pytest.mark.parametrize(
+    "tiles, constellations, root_dir, expected",
+    [
+        ([square_tile], ["sentinel-2"], "gs://oxeo-water/test", [sentinel2_tile_path]),
+        (
+            [square_tile],
+            ["sentinel-2", "landsat-8"],
+            "gs://oxeo-water/test",
+            [sentinel2_tile_path, landsat8_tile_path],
+        ),
+        (
+            [square_tile, square_tile],
+            ["sentinel-2", "landsat-8"],
+            "gs://oxeo-water/test",
+            [
+                sentinel2_tile_path,
+                landsat8_tile_path,
+                sentinel2_tile_path,
+                landsat8_tile_path,
+            ],
+        ),
+    ],
+)
+def test_make_paths_ok(tiles, constellations, root_dir, expected):
+    assert tile.make_paths(tiles, constellations, root_dir) == expected
