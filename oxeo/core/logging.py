2cfecb091456613ddf6a7b921b2bb64a0d372173
Chris Arderne <chris@rdrn.me>
2022-07-04 16:58:37 +0100

    Allow to change logging level

--- oxeo/core/logging.py
+++ oxeo/core/logging.py
@@ -4,12 +4,22 @@ import logging
 def get_logger() -> logging.Logger:
     """Initializes standard Logger that Prefect Cloud can pick up."""
 
-    logging.basicConfig(
-        format="%(asctime)s | %(levelname)8s | %(module)s:%(funcName)s:%(lineno)s - %(message)s",
-        level=logging.INFO,
-    )
-    logger = logging.getLogger("oxeo.water")
+    logger = logging.getLogger("oxeo")
+    if len(logger.handlers) == 0:
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)8s | %(module)s:%(funcName)s:%(lineno)s - %(message)s"
+        )
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
     return logger
 
 
 logger = get_logger()
+
+
+def set_level(level: int = 40) -> None:
+    """Defaults to ERROR, useful to run in notebooks."""
+    get_logger().setLevel(level)
